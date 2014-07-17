cd0899854c6dd42bd242cc88f05a2ede57f96ce5
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-17 07:46:07 -0500

    Add GRACE Libraires

---
+++ grace-cmd/grace-cmd.cpp
@@ -0,0 +1,6 @@
+#include "grace-cmd.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
---
+++ grace-cmd/grace-cmd.h
@@ -0,0 +1,6 @@
+#pragma once
+
+///*** STANDARD INCLUDES ***///
+#include <SDKDDKVer.h>
+#include <stdio.h>
+#include <tchar.h>
