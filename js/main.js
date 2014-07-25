be0fdf6934d0bca761d08514affc48429d947892
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-25 12:58:29 -0500

    Do A Tutorial

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var gl; //Global variable for WebGL context
+var canvas; //WebGL canvas for drawing
+
+// START EVERYTHING
+function start() {
+  init();
+}
+
+// INITIALIZE DRAWING
+function init() {
+  canvas = document.getElementById("glcanvas");
+  gl = initWebGL(canvas);
+  
+  //Only continue if WebGL is working
+  if (!gl) { return; }
+  
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); //clear screen
+  gl.enable(gl.DEPTH_TEST); //enable depth testing
+  gl.depthFunc(gl.LEQUAL); //near things obscure far things
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //clear screen and depth buffer
+  
+  //set viewport to maximum canvas width
+  gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+function initWebGL(canvas) {
+  gl = null;
+  
+  try {
+    //try to get the standard context.  If fails, fallback to experimental context
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  }
+  catch(e) {}
+  
+  //if getting GL context fails, give up now
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+    gl = null;
+  }
+  
+  return gl;
+}
