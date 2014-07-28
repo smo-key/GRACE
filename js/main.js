c83e9ca86c07087f215824c22b855ca7ea0c6916
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-28 17:08:38 -0500

    Add datGUI Timelapse controls TODO: Move to buttons in lower-left of screen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //*** GLOBALS ***//
 this.view = 'Frequency Map';
+this.year = 2014;
+this.month = 1;
+this.binsize = 2.0;
+this.run = false;
 
 //*** DATGUI ***//
 var gui = new dat.GUI({ autoPlace: false });
@@ -8,8 +12,18 @@ customContainer.appendChild(gui.domElement);
 
 var viewupdate = gui.add(this, 'view', [ 'Frequency Map', 'Gravity Map' ] ).name("View");
 viewupdate.onChange(function(value){
+  //change viewmodes
   
 });
+
+var sizeupdate = gui.add(this, 'binsize', 0.5, 10.0).name("Binsize (degrees)");
+
+var time = gui.addFolder("Time Lapse");
+var yearupdate = time.add(this, 'year', [2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012]).name("Year");
+var monthupdate = time.add(this, 'month',[1,2,3,4,5,6,7,8,9,10,11,12]).name("Month");
+var runupdate = time.add(this, 'run', false).name("Run Timelapse");
+
+
 
 //*** INITIALIZE THREE.JS ***//
 var renderer = new THREE.WebGLRenderer({
