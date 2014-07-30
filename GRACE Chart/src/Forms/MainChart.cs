7c3afcec98d7cee67a5a1ba09a3a94e9287ff718
ellisnaoki <ellisnaoki@gmail.com>
2014-07-30 15:57:35 -0500

    Chart Update 2

--- GRACE Chart/src/Forms/MainChart.cs
+++ GRACE Chart/src/Forms/MainChart.cs
@@ -8,14 +8,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GRACEdata;
-
+using System.IO.Pipes;
+using System.IO;
 namespace GRACEChart
 {
     public partial class MainChart : BaseForm
     {   
-        String[] GLDASData = System.IO.Directory.GetFiles("../../../../gracedata/modeltimeseries/GLDAS");
-        String[] RL05Data = System.IO.Directory.GetFiles("../../../../gracedata/modeltimeseries/RL05");
+        String[] GLDASData = System.IO.Directory.GetFiles("../../../../../gracedata/modeltimeseries/GLDAS");
+        String[] RL05Data = System.IO.Directory.GetFiles("../../../../../gracedata/modeltimeseries/RL05");
+        String[] GRACEData = System.IO.Directory.GetFiles("../../../../../gracedata/groundtrack");
 
+   
         public MainChart()
         {
             InitializeComponent();
@@ -40,17 +43,16 @@ namespace GRACEChart
                 showGRACE(g);
             }
         }
-
+        //*SHOW GRAPHS*//
         public void showGLDAS(Graphics g)
         {
+            //Color a = Color.FromArgb(a, r, g, b);
             System.Drawing.Color a = Color.Green;  
         }
-
         public void showRL05(Graphics g)
         {
             System.Drawing.Color b = Color.DarkBlue;
         }
-
         public void showGRACE(Graphics g)
         {
             System.Drawing.Color c = Color.Crimson;
@@ -60,20 +62,164 @@ namespace GRACEChart
         {
 
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void yAxis_Click(object sender, EventArgs e)
         {
 
         }
-
         private void GLDAS_CheckedChanged(object sender, EventArgs e)
         {
 
         }
+        
+        //*READ GLDAS DATA*// 
+        List<Point> gldas = new List<Point>();
+        public void readGLDAS()
+        {
+            foreach (String file in GLDASData)
+            {
+                StreamReader reader = new StreamReader(file);
+                while (!reader.EndOfStream)
+                {
+                    string s = reader.ReadLine();
+                    string[] parameters = s.Split(' ');
+                    int count = parameters.Length;
+
+                    double t = Convert.ToDouble(parameters[2]);
+                    DateTime time = new DateTime(0, DateTimeKind.Utc);
+                    time = time.AddYears(2007);
+                    time = time.AddHours(t);
+                    int ti = Convert.ToInt32(time);
+                    int height = (int)Convert.ToDouble(parameters[4]);
+                    Point p = new Point(ti, height);
+                    gldas.Add(p);
+                }
+            } 
+        }
+
+        
+        //*READ RL-05 DATA*//
+        List<Point> rl05 = new List<Point>();
+        public void readRL05()
+        {
+            foreach (String file in RL05Data)
+            {
+                StreamReader reader = new StreamReader(file);
+                while (!reader.EndOfStream)
+                {
+                    string s = reader.ReadLine();
+                    string[] parameters = s.Split(' ');
+                    int count = parameters.Length;
+
+                    double t = Convert.ToDouble(parameters[2]);
+                    DateTime time = new DateTime(0, DateTimeKind.Utc);
+                    time = time.AddYears(2007);
+                    time = time.AddHours(t);
+                    int ti = Convert.ToInt32(time);
+                    int height = (int)Convert.ToDouble(parameters[4]);
+                    Point p = new Point(ti, height);
+                    rl05.Add(p);
+                }
+            }
+        }
+        //*READ GRACE*//
+        List<Point> grace = new List<Point>();
+        List<Point> graceGLDAS = new List<Point>();
+        List<Point> graceRL05 = new List<Point>();
+        public void readGRACE()
+        {
+            foreach (string file in GRACEData)
+            {
+                StreamReader reader = new StreamReader(file);
+                while (!reader.EndOfStream)
+                {
+                     string s = reader.ReadLine();
+                     string[] parameters = s.Split(' ');
+                     int count = parameters.Length;
+
+                     double t = Convert.ToDouble(parameters[0]);
+                     DateTime time = new DateTime(0, DateTimeKind.Utc);
+                     time = time.AddYears(1999);
+                     time = time.AddHours(t);
+                     int ti = Convert.ToInt32(time);
+
+                     int height = 0;
+
+                     if (GLDAS.Checked)
+                     {
+                         foreach (string file2 in GLDASData)
+                         {
+                             StreamReader reader2 = new StreamReader(file);
+                             while (!reader.EndOfStream)
+                             {
+                                 string s2 = reader.ReadLine();
+                                 string[] parameters2 = s.Split(' ');
+                                 int count2 = parameters.Length;
+                                 height = (int)Convert.ToDouble(parameters[4]);
+                                 Point p = new Point(ti, height);
+                                 graceGLDAS.Add(p);
+                             }
+                         }
+                     }
+                     else if (RL05.Checked)
+                     {
+                         foreach (string file3 in RL05Data)
+                         {
+                             StreamReader reader3 = new StreamReader(file);
+                             while (!reader.EndOfStream)
+                             {
+                                 string s3 = reader.ReadLine();
+                                 string[] parameters3 = s.Split(' ');
+                                 int count3 = parameters.Length;
+                                 height = (int)Convert.ToDouble(parameters[4]);
+                                 Point p = new Point(ti, height);
+                                 graceRL05.Add(p);
+                             }
+                         }
+                     }
+                     else if (GLDAS.Checked && RL05.Checked)
+                     {
+                         foreach (string file2 in GLDASData)
+                         {
+                             StreamReader reader2 = new StreamReader(file);
+                             while (!reader.EndOfStream)
+                             {
+                                 string s2 = reader.ReadLine();
+                                 string[] parameters2 = s.Split(' ');
+                                 int count2 = parameters.Length;
+                                 height = (int)Convert.ToDouble(parameters[4]);
+                                 Point p = new Point(ti, height);
+                                 graceGLDAS.Add(p);
+                             }
+                         }
+                         foreach (string file3 in RL05Data)
+                         {
+                             StreamReader reader3 = new StreamReader(file);
+                             while (!reader.EndOfStream)
+                             {
+                                 string s3 = reader.ReadLine();
+                                 string[] parameters3 = s.Split(' ');
+                                 int count3 = parameters.Length;
+                                 height = (int)Convert.ToDouble(parameters[4]);
+                                 Point p = new Point(ti, height);
+                                 graceRL05.Add(p);
+                             }
+                         }
+                         
+                     }
+                     else
+                     {
+                         height = 0;                     
+                         Point p = new Point(ti, height);
+                         grace.Add(p);
+                     }
+
+                }
+            }
+        }
+
     }
 }
