13c5d67efd76c6dd31854e6505e5507f553d003b
Arthur Pachachura <pachachura.arthur@gmail.com>
2014-07-30 19:26:28 -0500

    Add GRACE JSON Converter

---
+++ GRACE JSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GRACEdata;
+
+namespace GRACE_JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GRACE Data Automated JSON Converter");
+            Console.Write("Continuing will DELETE all currently created data! [y/n] ");
+            if (Console.ReadKey().ToString() != "y") { return; }
+
+            Structs.PointTime lasttime = new Structs.PointTime();
+            Structs.Anchor anchor = Structs.Anchor.Uniform;
+            if (!(360 % Globals.gridsize == 0)) { anchor = Structs.Anchor.Center; }
+
+
+
+        }
+    }
+}
